f7c0c30851c0a0ccc6a69a26598ab19d67ec070e
Francesco Straniero <158042905+frastra2004@users.noreply.github.com>
2024-12-16 11:47:06 +0100

    Add files via upload

--- adc_filter.py
+++ adc_filter.py
@@ -1,9 +1,19 @@
-def adc_filter(array,coeff):
+import numpy as np
+def adc_filter_func(array,coeff):
     ph = array.copy()
     for j in range(2):
         accu = ph[0]
         for i in range(len(ph)):
-            accu = accu+ (coeff[j]*(ph[i] - accu))
+            accu = accu + coeff[j]*(ph[i] - accu)
             ph[i] = accu
-            
+    
+    noise = np.random.normal(0,5,int((len(ph)/4)))
+    s = np.array([np.zeros(int(len(ph)/4)),noise])
+    noise = np.ravel(s,order='F')
+    
+    s1 = np.array([np.zeros(len(noise)),noise])
+    noise = np.ravel(s1, order= 'F')
+
+
+    ph = np.add(ph,noise)
     return ph
